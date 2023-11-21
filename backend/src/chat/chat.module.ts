a54da59775209b687128f5f4efbf6f7d068a370f
Nasreddine Hanafi <nhanafi@e3r11p9.1337.ma>
2023-11-21 11:31:58 +0100

    duo

--- backend/src/chat/chat.module.ts
+++ backend/src/chat/chat.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { GroupsController } from './controllers/groups/groups.controller';
 import { GroupsService } from './services/groups/groups.service';
+import { DuoController } from './controllers/chats/chats.controller';
+import { DuoService } from './services/chats/chats.service';
 
 @Module({
-  controllers: [GroupsController],
-  providers: [PrismaService, GroupsService]
+  controllers: [GroupsController, DuoController],
+  providers: [PrismaService, GroupsService, DuoService],
+  exports: [DuoService]
 })
 export class ChatModule {
 }
--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -6,10 +6,11 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { UserService } from './services/user.service';
 import { FriendService } from './services/friend.service';
 import { BlockController } from './controllers/block/block.controller';
+import { DuoService } from 'src/chat/services/chats/chats.service';
 
 @Module({
   controllers: [UserController, FriendController, BlockController],
-  providers: [FriendService, UserService, PrismaService, BlockService]
+  providers: [FriendService, UserService, PrismaService, BlockService, DuoService],
 })
 export class UserModule {
 }
