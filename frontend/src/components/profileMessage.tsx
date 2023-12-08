1193c4caad2d2a021ac6ba48b4a9b9fe3ec29e68
Brahim-maaqoul <wacakoule@gmail.com>
2023-12-08 21:35:10 +0100

    adding update channel component

--- frontend/src/components/profileMessage.tsx
+++ frontend/src/components/profileMessage.tsx
@@ -6,18 +6,23 @@ import Image from "next/image";
 import { close } from "./toggle";
 import NextImage from "next/image";
 import Link from "next/link";
-import { usegetGroups } from "@/app/api/chatApi/chatApiFunctions";
+import {
+  useGetMemberShip,
+  usegetGroups,
+} from "@/app/api/chatApi/chatApiFunctions";
 import { group } from "console";
 import { Chat } from "./Groups";
 import { spinner } from "@/app/[user]/profile/page";
 import { io } from "socket.io-client";
 import { Socket } from "dgram";
+import { CreatGroup } from "./creatGroup";
+import { UpdateChannel } from "./updateChannel";
 
 interface ProfileMessagesProps {
   group: any;
   more: boolean;
   setMore: React.Dispatch<React.SetStateAction<boolean>>;
-  socket: any
+  socket: any;
   // data: MessageInfo[];
   // setData: React.Dispatch<React.SetStateAction<MessageInfo[]>>;
   // dataUser: any;
@@ -27,21 +32,27 @@ export const ProfileMessages: React.FC<ProfileMessagesProps> = ({
   group,
   more,
   setMore,
-  socket
+  socket,
 }) => {
   const [isTyping, setisTyping] = useState<string | null>("");
-  const { dataUser, showFalse } = useAuth();
-  socket.on("isTyping", (data: {message: string, id: number}) => {
-    console.log("isTyping111", data, group.id)
-    if (data.id === Number(group.id))
-      setisTyping(data.message)
-  })
+  const [newGroup, setNewGroup] = useState(false);
+
+  const { showFalse } = useAuth();
+  const { data: getMembership } = useGetMemberShip(group.id);
+  socket.on("isTyping", (data: { message: string; id: number }) => {
+    console.log("isTyping111", data, group.id);
+    if (data.id === Number(group.id)) setisTyping(data.message);
+  });
   socket.on("notTyping", () => {
-    setisTyping(null)
-    console.log("baddrr")
-  })
+    setisTyping(null);
+    console.log("baddrr");
+  });
+
   return (
-    <div className="w-full h-1/10 border-b border-gray-300 flex items-center p-1">
+    <div className=" w-full h-1/10 border-b border-gray-300 flex items-center p-1">
+      {newGroup && (
+        <UpdateChannel newGroup={newGroup} setNewGroup={setNewGroup} />
+      )}
       <div className="flex">
         {group.type === "duo" ? (
           <Link href={"/" + group.members[0].user.nickname + "/profile"}>
@@ -74,25 +85,37 @@ export const ProfileMessages: React.FC<ProfileMessagesProps> = ({
         )}
       </div>
 
-      <button className="ml-auto mr-0" onClick={() => setMore(true)}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="text-gray-300 hover:text-white icon icon-tabler icon-tabler-info-circle"
-          width="28"
-          height="28"
-          viewBox="0 0 24 24"
-          strokeWidth="2"
-          stroke="currentColor"
-          fill="none"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-          <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0"></path>
-          <path d="M12 9h.01"></path>
-          <path d="M11 12h1v4h1"></path>
-        </svg>
-      </button>
+      <div className="ml-auto mr-0 flex items-center">
+        {getMembership?.type === "creator" && (
+          <button className="mr-3" onClick={() => setNewGroup(true)}>
+            <div className="w-6 h-6">
+              <div
+                className="h-6 w-6 rounded-full bg-cover"
+                style={{ backgroundImage: `url(/edit.png)` }}
+              ></div>
+            </div>
+          </button>
+        )}
+        <button onClick={() => setMore(true)}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="text-gray-300 hover:text-white icon icon-tabler icon-tabler-info-circle"
+            width="28"
+            height="28"
+            viewBox="0 0 24 24"
+            strokeWidth="2"
+            stroke="currentColor"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0"></path>
+            <path d="M12 9h.01"></path>
+            <path d="M11 12h1v4h1"></path>
+          </svg>
+        </button>
+      </div>
 
       <button className="ml-4 flex lg:hidden" onClick={showFalse}>
         {close}
