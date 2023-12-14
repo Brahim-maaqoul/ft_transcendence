e11c7826ac48a2444ec2a157042c509fb335763d
Brahim  Maaqoul <wacakoule@gmail.com>
2023-12-15 00:21:32 +0100

    adding stuff

--- frontend/src/components/stats.tsx
+++ frontend/src/components/stats.tsx
@@ -1,27 +1,28 @@
 import Image from "next/image";
 import { UserProfile, useAuth } from "./providers/AuthContext";
 import { useGetStatsUser } from "@/app/api/getStats";
+import Link from "next/link";
 
 function Stats({ profileData }: { profileData: UserProfile }) {
   const { dataUser } = useAuth();
-  const {data, isLoading, isError} = useGetStatsUser(profileData.auth_id);
-  console.log(data)
-  if (isLoading)
-    return <></>
-  if (isError)
-    return <></>
+  const { data, isLoading, isError } = useGetStatsUser(profileData.auth_id);
+  console.log(data);
+  if (isLoading) return <></>;
+  if (isError) return <></>;
   return (
     <div className="bg-black bg-opacity-40 rounded-2xl md:shadow-black shadow-2xl flex flex-col p-4 mx-2 my-3">
       <span className="text-white text-2xl font-mono">Stats</span>
       <div className="flex gap-x-6 my-4">
         <div className="flex flex-col items-center w-2/3">
-          <div className="w-20 h-20 flex items-center">
-            <div
-              className="h-20 w-20 rounded-full bg-cover"
-              style={{
-                backgroundImage: `url(${profileData?.picture})`,
-              }}></div>
-          </div>
+          <Link href={"/profile/" + profileData?.nickname}>
+            <div className="w-20 h-20 flex items-center">
+              <div
+                className="h-20 w-20 rounded-full bg-cover"
+                style={{
+                  backgroundImage: `url(${profileData?.picture})`,
+                }}></div>
+            </div>
+          </Link>
           <span className="text-white text-xl">{profileData?.nickname}</span>
         </div>
         <div className="flex flex-col items-center w-3/4">
@@ -42,13 +43,15 @@ function Stats({ profileData }: { profileData: UserProfile }) {
           </div>
         </div>
         <div className="flex flex-col items-center w-2/3">
-          <div className="w-20 h-20 flex items-center">
-            <div
-              className="h-20 w-20 rounded-full bg-cover"
-              style={{
-                backgroundImage: `url(${dataUser?.picture})`,
-              }}></div>
-          </div>
+          <Link href={"/profile/" + dataUser?.nickname}>
+            <div className="w-20 h-20 flex items-center">
+              <div
+                className="h-20 w-20 rounded-full bg-cover"
+                style={{
+                  backgroundImage: `url(${dataUser?.picture})`,
+                }}></div>
+            </div>
+          </Link>
           <span className="text-white text-xl">{dataUser?.nickname}</span>
         </div>
       </div>
